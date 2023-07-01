6a60a73410012650af1ed9a9c98d2a0348afdb8d
David Omsiakin <davidomisakin4good@gmail.com>
2023-07-01 20:14:20 +0100

    Fixed the favicon,the navbar links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          {/* <Route path='foreclosures/' element={<Foreclosures/>} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import '../css/navbar.css'
 import logo from '../images/z-logo-default.svg'
 // import {NavLink} from 'react-router-dom';
+// import { NavLink, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 function NavBar() {
   return (
     <div>
@@ -9,22 +11,22 @@ function NavBar() {
             <nav>
                 <div className='nav-ul1'>
                     <ul>
-                        <li>Buy</li>
-                        <li>Rent</li>
-                        <li>Sell</li>
-                        <li>Home Loans</li>
-                        <li>Agent finder</li>
+                        <li><Link> Buy </Link></li>
+                        <li><Link> Rent </Link></li>
+                        <li><Link> Sell </Link></li>
+                        <li><Link> Home Loans </Link></li>
+                        <li><Link> Agent finder </Link></li>
                     </ul>
                 </div>
                 <div className='nav-img'>
-                    <img src={logo} alt='logo'/>
+                   <Link to={'/'}> <img src={logo} alt='logo'/></Link>
                 </div>
                 <div className='nav-ul2'>
                     <ul>
-                        <li>Manage Rentals</li>
-                        <li>Advertise</li>
-                        <li>Help</li>
-                        <li>Sign in</li>
+                        <li><Link>Manage Rentals </Link></li>
+                        <li><Link>Advertise </Link></li>
+                        <li><Link>Help </Link></li>
+                        <li><Link>Sign in </Link></li>
                     </ul>
                 </div>
             </nav>
