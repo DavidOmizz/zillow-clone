a69f681c6dc8db7c34470f6ef9563ceb09f64d2f
David <davidomisakin4good@gmail.com>
2023-12-20 09:28:30 +0100

    To test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import {BiSearch} from 'react-icons/bi'
 function Hero() {
   return (
     <div className='hero-container'>
-        <h1>Agents. Tours. Loans. Homes.</h1>
+        <h1>wertAgents. Tours. Loans. Homes.</h1>
         <div class="search-container">
             <input type="text" id="search-input" placeholder="Enter an address, neighborhood, city, or ZIP code"/>
             <span class="search-icon"><BiSearch/></span>
